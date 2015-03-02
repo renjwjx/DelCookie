5086d9311b117d0216604181147a60808725af9c
jinren <jinren@jinrens-iMac.local>
2015-03-02 14:24:01 +0800

    Initial Commit

---
+++ DelCookie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelCookie
+//
+//  Created by jinren on 3/2/15.
+//  Copyright (c) 2015 imp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DelCookie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DelCookie
+//
+//  Created by jinren on 3/2/15.
+//  Copyright (c) 2015 imp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
